454b1b8d8282a05ef595a60d3f5dc6b4b9c8ab57
ankitraju123 <ankitrchauhan1997@gmail.com>
2023-01-12 12:42:09 +0530

    add

---
+++ naukri.js
@@ -0,0 +1,47 @@
+var data = [
+    {
+      img_url: "https://img.naukimg.com/logo_images/v2/892542.gif",
+      company: "TCS",
+    },
+    {
+      img_url: "https://img.naukimg.com/logo_images/v2/1384.gif",
+      company: "Wipro",
+    },
+    {
+      img_url: "https://img.naukimg.com/logo_images/v2/10081.gif",
+      company: "ICICI Bank",
+    },
+    {
+      img_url: "https://img.naukimg.com/fc_images/v2/16987.gif",
+      company: "IBM",
+    },
+    {
+      img_url: "https://img.naukimg.com/logo_images/v2/11014.gif",
+      company: "intel",
+    },
+  ];
+  
+ 
+ 
+  data.map(function (elem) {
+    var div = document.createElement("div");
+    var company = document.createElement("p");
+    company.innerText = elem.company;
+    var img = document.createElement("img");
+    img.src = elem.img_url;
+  
+    div.append(img, company);
+    document.getElementById("company").append(div);
+  });
+ 
+  document.querySelector("#navEmploy").addEventListener("click", slectFun);
+  function slectFun() {
+    event.preventDefault();
+    let a = document.querySelector("#navEmploy").value;
+    // console.log(a)
+    if (a === "Employer Login") {
+      window.location.href = "employlogin.html";
+    } else {
+      return false;
+    }
+  }
